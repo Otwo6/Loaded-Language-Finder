f7914018260722fb03cb17c0baa02bef146aed36
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-09-10 09:35:28 -0700

    Include Phrases in CSV and that ability to spot phrases

--- popup/popup.js
+++ popup/popup.js
@@ -11,6 +11,8 @@ let rows = [];
 
 let bias = 0;
 
+let currentWordChecking = 0; // Word that is currently being checked
+
 function loadCSV() {
     fetch(chrome.runtime.getURL('csvFile.csv'))
     .then(response => response.text())
@@ -33,24 +35,64 @@ function checkText(passage) {
     fixedPassage = "";
     splitText(passage);
     
-    for(let i = 0; i < split.length; i++) {
-        checkCSV(split[i]);
+    currentWordChecking = 0;
+    while(currentWordChecking < split.length) {
+        console.log("Checking: " + split[currentWordChecking]);
+        checkCSV(currentWordChecking);
+        currentWordChecking++;
     }
     fixedIDElement.value = fixedPassage;
     console.log("Bias words: " + bias);
 }
 
-function checkCSV(word) {
+function checkCSV(splitNumber) {
     for(let i = 0; i < rows.length; i++) {
         const columns = rows[i].split(','); // Split each row into columns
-        if (columns[0].toLowerCase() === word.toLowerCase()) { // Check if the word is in column A (index 0)
-            console.log("Word Split: " + word);
-            word = columns[1].replace(/(\r\n|\n|\r)/gm,"");
+        
+        let csvWord = columns[0].toLowerCase().split(' ');
+        if(csvWord.length > 1)  // Seperate phrases and words in csv file
+        {
+            // Is a phrase
+
+            let hasPhrase = true;
+
+            for(let i = 0; i < csvWord.length; i++)
+            {
+                if(csvWord[i] == split[splitNumber + i])
+                {
+                }
+                else
+                {
+                    hasPhrase = false;
+                    break;
+                }
+            }
+
+            if(hasPhrase)
+            {
+                currentWordChecking = currentWordChecking + csvWord.length - 1;
+                console.log("Contains phrase: " + columns[0].toLowerCase());
+
+                let correctPhrase = columns[1].toLowerCase().split(' ');
+                for(let i = 0; i < csvWord.length; i++) {
+                    split[splitNumber + i] = "";
+                }
+                split[splitNumber] = columns[1].replace(/(\r\n|\n|\r)/gm,"");
+            }
+        }
+        else
+        {
+            // Is a word 
+        }
+
+        if (columns[0].toLowerCase() === split[splitNumber].toLowerCase()) { // Check if the word is in column A (index 0)
+            console.log("Word Split: " + split[splitNumber]);
+            split[splitNumber] = columns[1].replace(/(\r\n|\n|\r)/gm,"");
             bias++;
             break;
         }   
     }
-    fixedPassage = fixedPassage.concat(word + " ");
+    fixedPassage = fixedPassage.concat(split[splitNumber] + " ");
 }
 
 loadCSV();
